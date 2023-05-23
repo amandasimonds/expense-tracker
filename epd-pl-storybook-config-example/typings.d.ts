517d11381121f891ca4c9183e313c348aad6eb47
Amanda Simonds <amandasimonds9@gmail.com>
2023-05-23 13:03:56 -0400

    initial commit

---
+++ epd-pl-storybook-config-example/typings.d.ts
@@ -0,0 +1,9 @@
+declare module '*.md' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.mdx' {
+    let MDXComponent: (props: any) => JSX.Element;
+    export default MDXComponent;
+}
