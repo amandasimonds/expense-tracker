7063eb7767e6bc27ee8fbc225d9f808f4ab9727d
Amanda Simonds <amandasimonds9@gmail.com>
2023-06-29 13:17:36 -0400

    you can conditionally return different JSX content. god i love ya react! so lean :D

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -3,18 +3,11 @@ import './ExpensesList.scss';
 import ExpenseItem from './ExpenseItem';
 
 function ExpensesList(props) {
-	let expensesContent = <p>No expenses found.</p>;
 
-	if (props.items.length > 0) {
-		expensesContent = props.items.map((item) => (
-			<ExpenseItem
-				key={item.id}
-				title={item.title}
-				amount={item.amount}
-				date={item.date}
-			/>
-		));
+	if (props.items.length === 0) {
+		return <h2 className='expenses-list__fallback'>Found no expenses.</h2>
 	}
+
 	return (
 		<ul className="expenses-list">
 			{props.items.map((item) => (
